9a3b3687c921d2d08c34e7af1a760529a5e3145f
LuisFernandoAlmeidaNunes <luis_jna@outlook.com>
2025-12-09 16:41:15 -0300

    feat: burr scipy substituida por burr_fast

--- simulador.py
+++ simulador.py
@@ -1,20 +1,25 @@
 import matplotlib.pyplot as plt
-import scipy
 import numpy as np
 
 class Simulacao:
 
-    def inicia_frota(self, mean, sigma):
+    def inicia_motorista(self):
         # inicializando motorista
         motorista = {
             "disponivel" : 1
         }
+        
+        return motorista
+
+    def inicia_caminhao(self, chance_defeito):
         # inicializando caminhao
         caminhao = {
             "disponivel" : 1,
-            "chance_defeito" : 5 
+            "chance_defeito" : chance_defeito 
         }
 
+        return caminhao
+    
     #Primeira parte do DCA
     def inicia_pedido(self):
 
@@ -41,17 +46,17 @@ class Simulacao:
 
         return pedido
 
-    #sorteio do tempo de deslocamento em meio urbano por um caminhão
+    # implementacao da pert, numpy + simulacao de distribuicoes
     def pert(self, o, m, p, lamb):
 
             alpha = 1 + lamb * ((m - o) / (p - o))
             beta = 1 + lamb * ((p - m) / (p - o))
             
-            x = np.random.beta(alpha, beta, 0, 1)
+            x = np.random.beta(alpha, beta, size=None)
 
-            tempo = o + x * (p - o)
-
-            return  tempo
+            return  o + x * (p - o)
+    
+    #sorteio do tempo de deslocamento em meio urbano por um caminhão
     def sorteia_tempo_urbano(self, mean, sigma, chance_problema):
         
         # tempo de deslocamento em cidade
@@ -67,22 +72,24 @@ class Simulacao:
     def sorteia_tempo_rodoviario(self, c, d, scale, chance_problema, chance_parada):
         
         # tempo de deslocamento em rodovia
-        tempo = scipy.stats.burr(c, d, loc=0, scale=scale)
+        tempo = float(burr_fast(c, d))
 
         # chance de algum imprevisto ocorrer no deslocamento em rodovia
         if np.random.randint(1, 100) < chance_problema:
-            tempo += np.random.lognormal(mean=120, sigma=60, size=None)
-
+            problema = np.random.lognormal(mean=1, sigma=0.5, size=None)
+            tempo += problema
         # chance do nó ser uma parada
         if np.random.randint(1, 100) < chance_parada:
-            tempo += self.sorteia_parada()
 
-        return tempo
+            parada = self.sorteia_parada(0.5, 1, 2)
+            print(f"parada = {parada}")
+            tempo += parada
+        return (tempo, problema, parada)
     
     def sorteia_parada(self, tempo_o, tempo_m, tempo_p):
 
         # tempo de manobra + planejamento
-        tempo = 45
+        tempo = 0.45
 
         #sorteia tempo de descarga
         tempo += self.pert(tempo_o, tempo_m, tempo_p, 4)
@@ -95,7 +102,7 @@ class Simulacao:
     def sorteia_carga(self, tempo_o, tempo_m, tempo_p):
 
         # tempo de manobra + planejamento
-        tempo = 45
+        tempo = 0.45
 
         #sorteia tempo de carga
         tempo = self.pert(tempo_o, tempo_m, tempo_p, 4)
@@ -106,17 +113,36 @@ class Simulacao:
     def sorteia_descarga(self, tempo_o, tempo_m, tempo_p):
 
         # tempo de manobra + planejamento
-        tempo = 45
+        tempo = 0.45
 
         #sorteia tempo de descarga
         tempo = self.pert(tempo_o, tempo_m, tempo_p, 4)
     
         return tempo
+    
+    
+def burr_fast(c, k):
+    u = np.random.random()
+    return ((1 - u) ** (-1 / k) - 1) ** (1 / c)
+
+def plotar_distribuicao(dados):
+    plt.figure(figsize=(12, 6))
+
+    plt.hist(dados, bins=100, density=True, alpha=0.5, label="")
+
+    plt.title("Comparação das distribuições")
+    plt.xlabel("Valor")
+    plt.ylabel("Densidade")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 if __name__ == "__main__":
     s = Simulacao()
     
-    print(s.inicia_pedido())
+    # print(s.sorteia_tempo_rodoviario(2, 3, 1, 10, 40))
+    
 
-    while len(a) < 100:
-        a.append(s.inicia_pedido())
+
+    # while len(a) < 100:
+    #     a.append(s.inicia_pedido())
